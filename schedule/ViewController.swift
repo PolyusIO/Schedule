47d0e9a3b748096e14bc077c402679d94e0fa898
Sergey Polyakov <PolyusIO@yandex.ru>
2023-07-30 18:05:36 +0500

    Initial Commit

---
+++ schedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  schedule
+//
+//  Created by Сергей Поляков on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
